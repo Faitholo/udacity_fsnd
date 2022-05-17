66f0eab1eae305621bd59608cba03131a9b06843
Faith Ikhidero <ikhidero.faithy@gmail.com>
2022-05-17 10:46:49 +0100

    made some updates

--- databases/flask/app.py
+++ databases/flask/app.py
@@ -2,12 +2,28 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://faith@localhost:5432/test'
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///test'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+class Person(db.Model):
+    """Creating a persons table"""
+    __tablename__ = 'persons'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f'<ID: {self.id}, name: {self.name}>'
+# Calling the create_all() method   
+db.create_all()
 
 @app.route('/')
 def greet():
-    return 'Hello World!'
+    person = Person.query.first()
+    return 'Hello ' + person.name
 
 if __name__ == '__main__':
-   app.run(host="0.0.0.0")
+    app.debug = True
+    app.run(host="0.0.0.0")
