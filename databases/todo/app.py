e17dbebcb18ce2eeace7239c44f9e8898be79983
Faith Ikhidero <ikhidero.faithy@gmail.com>
2022-05-19 18:44:54 +0100

    starting out with CRUD

---
+++ databases/todo/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
+from flask_sqlalchemy import SQLAlchemy
+from flask import request
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///todos'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+  __tablename__ = 'todo_lists'
+  id = db.Column(db.Integer, primary_key=True)
+  description = db.Column(db.String(), nullable=False)
+
+  def __repr__(self):
+    return f'<Todo {self.id} {self.description}>'
+
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+  description = request.form.get('description')
+  todo = Todo(description=description)
+  db.session.add(todo)
+  db.session.commit()
+  return render_template('index.html', data=Todo.query.all())
+
+
+
+@app.route('/')
+def index():
+  return render_template('index.html', data=Todo.query.all())
+
+
+
+if __name__ == '__main__':
+   app.run(host="0.0.0.0", port=3000)
