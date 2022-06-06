8923148db1f057e2d8e273c4600f4d79326d7b9e
Faith Ikhidero <ikhidero.faithy@gmail.com>
2022-06-06 22:04:07 +0100

    updating ...

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,25 +1,28 @@
-from flask import Flask, jsonify
-#from models import setup_db, Plant
-from flask_cors import CORS
+from flask import Flask, jsonify, request
+from .models import setup_db, Plant
+#from flask_cors import CORS
 
 def create_app(test_config=None):
     # Create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    CORS(app)
-    
+    app = Flask(__name__)
+    setup_db(app)
+    #CORS(app)
+    """
     @app.after_request
     def after_request(response):
         response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
         response.headers.add('Access-Control-Allow-Headers', 'GET, POST, PATCH, DELETE, OPTIONS')
         return response
+    """
+    @app.route('/plants', methods=['GET', 'POST'])
+    def list_plants():
+        plants = Plant.query.all()
+        formatted_plants = [plants.format() for plant in plants]
+        
+        return jsonify({
+            'success':True,
+            'plants':formatted_plants
+        })
     
-    @app.route('/')
-    def hello():
-        return "Hello World!"
     
-    @app.route('/name')
-    def name():
-        return jsonify({'name': 'Faith'})
-    
-    
-    return app
+        return app
